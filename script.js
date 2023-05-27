9c7389ad56888b65aa4f47f211be737ba9af6447
ehliexplore <lgspotifyemail@gmail.com>
2023-05-27 14:53:52 -0300

    update
    Adicionando funções em javascript para todos os botões e outras melhorias.

--- script.js
+++ script.js
@@ -2,3 +2,38 @@ document.getElementById("spotify-button").addEventListener("click", function() {
     window.location.href = "https://open.spotify.com/album/3iDydf0ZyPYDEDmBcp8yg6"
 })
 
+document.getElementById("apple-music-button").addEventListener("click", function() {
+    window.location.href = "https://music.apple.com/br/album/se-um-dia-eu-desaparecer-feat-helo-ily-single/1687374900"
+})
+
+document.getElementById("deezer-button").addEventListener("click", function() {
+    window.location.href = "https://www.deezer.com/br/album/440040997"
+})
+
+document.getElementById("youtube-music-button").addEventListener("click", function() {
+    window.location.href = "https://music.youtube.com/playlist?list=OLAK5uy_mX-2Ig02Kbfe-1bw3DhHcFoa4xPzDbMxg"
+})
+
+document.getElementById("youtube-button").addEventListener("click", function() {
+    window.location.href = "https://www.youtube.com/watch?v=_ecDJz30RFs"
+})
+
+document.getElementById("itunes-store-button").addEventListener("click", function() {
+    window.location.href = "https://music.apple.com/br/album/se-um-dia-eu-desaparecer-feat-helo-ily-single/1687374900?at=11lqQE&ct=FFM_6a4c5eb979b0146577e2f141945205b0&ls=1&uo=4"
+})
+
+document.getElementById("amazon-music-button").addEventListener("click", function() {
+    window.location.href = "https://music.amazon.com/albums/B0C4WCWW5B?ref=dm_ff_featurefm"
+})
+
+document.getElementById("tidal-button").addEventListener("click", function() {
+    window.location.href = "https://tidal.com/browse/album/294259932"
+})
+
+document.getElementById("boomplay-button").addEventListener("click", function() {
+    window.location.href = "https://www.boomplay.com/albums/69772501?srModel=openapi_featurefm&ffm=FFM_82cf8631ed63e1ff2abd029b9341e6c0"
+})
+
+document.getElementById("claro-musica-button").addEventListener("click", function() {
+    window.location.href = "https://www.claromusica.com/getCountryForAnonymousLogin?redirectTo=/album/37735750/MX"
+})
